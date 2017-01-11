41ef112b01d4537454c51b76d8061af5eeb62e40
Andres Guijarro <aguijarro@vic-data.com>
2017-01-11 12:24:30 -0500

    feat: Add scripts which analyced data using naive bayes algorithm. Source: Udacity

---
+++ MachineLearning/Sklearn/naive_bayes/ex2/studentCode.py
@@ -0,0 +1,19 @@
+from prep_terrain_data import makeTerrainData
+from classify import NBAccuracy
+
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+
+def submitAccuracy():
+    accuracy = NBAccuracy(features_train, labels_train,
+                          features_test, labels_test)
+    return accuracy
+
+
+def main():
+    accuracy = submitAccuracy()
+    print (accuracy)
+
+if __name__ == '__main__':
+    main()
